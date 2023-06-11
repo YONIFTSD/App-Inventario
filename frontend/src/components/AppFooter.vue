2a9eb744bbf5e0ae15862f3b6a9556cc7fe664af
Yonathan William Mamami Calisaya <42424755+YONIFTSD@users.noreply.github.com>
2023-06-11 11:24:34 -0500

    correciones

--- frontend/src/components/AppFooter.vue
+++ frontend/src/components/AppFooter.vue
@@ -1,14 +1,13 @@
 <template>
   <CFooter>
     <div>
-   
+
       <span class="ms-1"
         >&copy; {{ new Date().getFullYear() }}</span
       >
     </div>
     <div class="ms-auto">
-      <span class="me-1" target="_blank">Desarrollado Por</span>
-      <a href="">Yonathan William M. C.</a>
+
     </div>
   </CFooter>
 </template>
